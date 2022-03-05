30e659355137d48b9e012513f12016de51499b39
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-03-04 23:56:56 -0600

    coming up with functions or ideas for functionality

--- js/configurations.js
+++ js/configurations.js
@@ -3,5 +3,23 @@ function start () {
 }
 
 function spin () {
+    // when the button is pressed this function should start
 
 }
+
+// need a destination to store names?
+// need a way to grab those names and dissect them
+// need an onclick functionality
+
+// ---- Getting random array value? ----
+// var names = [];
+// var randName = names[Math.floor(Math.random() * names.length)];
+
+// -- OR --
+
+// function randomName(names) {
+//     for (let i = 0; i < 2; i++) {
+//         const random = names[Math.floor(Math.random() * names.length)];
+//         return random
+//     }
+// }
